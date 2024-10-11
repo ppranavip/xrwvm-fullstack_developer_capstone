355e13e13577f6abb3c99ef68b271571998b8ef7
ppranavip <ppranavip@gmail.com>
2024-10-11 10:28:29 -0400

    proxy task

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from .restapis import get_request, analyze_review_sentiments, post_review
 from . import views
 
 app_name = 'djangoapp'
@@ -17,7 +18,13 @@ urlpatterns = [
     # path for get_cars
     path(route='get_cars', view=views.get_cars, name ='getcars'),
     # path for dealer reviews view
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    
 
     # path for add a review view
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    path(route='add_review', view=views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
